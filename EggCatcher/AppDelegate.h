9b4b1ae42cf8cb0a58c81f373415a8468a1015b1
Marc Henderson <marchend@gmail.com>
2015-05-12 17:36:31 -0400

    initial commit of new project
    Signed-off-by: Marc Henderson <marchend@gmail.com>

---
+++ EggCatcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EggCatcher
+//
+//  Created by Marc Henderson on 2015-05-12.
+//  Copyright (c) 2015 Marc Henderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EggCatcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EggCatcher
+//
+//  Created by Marc Henderson on 2015-05-12.
+//  Copyright (c) 2015 Marc Henderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
